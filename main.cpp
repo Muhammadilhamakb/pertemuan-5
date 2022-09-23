f6ee60c8ed146337487262b2b020c1bad5b83439
Muhammadilhamakb <112915107+Muhammadilhamakb@users.noreply.github.com>
2022-09-24 01:45:16 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello Wordl!" << endl;
+    cout << "my name is MUHAMMAD ILHAM AKBAR" << endl;
+
+    int u = 17;
+    float t = 164;
+    char b = 'L';
+    string k = "demak";
+    cout << endl;
+    cout << "Umur:" << u << endl;
+    cout << "Tinggi badan:" << t << endl;
+    cout << "Ukuran baju:" << b << endl;
+    cout << "Kota asal:" << k << endl;
+
+    return 0;
+}
